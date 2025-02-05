12fcec8e80f2de1a41e5f9223cc291c4b070cc8e
OLUWABORIOV <104468419+OLUWABORIOV@users.noreply.github.com>
2025-02-05 15:29:29 +0100

    commit

---
+++ movie/src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "191412e60844c046036f386ed079b8dc";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`
+);
+    const data = await response.json()
+    return data.results
+}
